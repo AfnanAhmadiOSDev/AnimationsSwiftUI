dd98bf5ed4a68df90bce6bc198513d11124eccea
Afnan Ahmad <afnan.ahmad.iosdev@gmail.com>
2024-02-28 11:14:09 +0500

    Initial Commit

---
+++ AnimationsSwiftUI/AnimationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUIApp.swift
+//  AnimationsSwiftUI
+//
+//  Created by Afnan Ahmad on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
