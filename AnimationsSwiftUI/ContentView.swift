29cad1d7a94f233823797a0c29379b974bc255e9
Afnan Ahmad <afnan.ahmad.iosdev@gmail.com>
2024-02-28 11:52:08 +0500

    1st portion of animations on button done

--- AnimationsSwiftUI/ContentView.swift
+++ AnimationsSwiftUI/ContentView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        Text("hello")
+        
+        Button("Tap Me") {
+            //animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        //.scaleEffect(animationAmount)
+        //.blur(radius: (animationAmount - 1) * 3)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        //.animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationAmount)
+        .onAppear(perform: {
+            animationAmount = 2
+        })
+        
     }
 }
 
